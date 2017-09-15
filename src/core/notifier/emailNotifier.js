dbadc140333a1b328c885e3a2df98cf37686b197
Tiaraju <tiaraju@vtex.com.br>
2017-09-15 16:50:51 -0300

    adding simplest conf to email sender

--- src/core/notifier/emailNotifier.js
+++ src/core/notifier/emailNotifier.js
@@ -1,4 +1,28 @@
+var mail = require('nodemailer');
 
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'noreply@gmail.com',
+    pass: 'stupidpass'
+  }
+});
+
+var mailOptions = {
+  from: 'noreply@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
 
 var doNotify = function function_name (argument) {
 	// body... 
